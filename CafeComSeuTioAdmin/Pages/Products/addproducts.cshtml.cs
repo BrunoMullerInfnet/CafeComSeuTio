5363f84b5843ff666816a731b121b60f4a1c9f3c
bruno.crespo <bruno.crespo@al.infnet.edu.br>
2025-05-14 08:21:21 -0300

    Olavo

--- CafeComSeuTioAdmin/Pages/Products/addproducts.cshtml.cs
+++ CafeComSeuTioAdmin/Pages/Products/addproducts.cshtml.cs
@@ -1,19 +1,29 @@
+using CafeComSeuTioAdmin.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CafeComSeuTioAdmin.Pages.Products
 {
-    public class addproductsModel : PageModel
+    public class addproductModel : PageModel
     {
         [BindProperty]
-        public string NomeProduto { get; set; }
+        public Product newProduct { get; set; }
+
         public void OnGet()
         {
         }
 
         public void OnPost()
         {
+            if (ModelState.IsValid)
+            {
+                var fileName = $"note-{DateTime.Now:yyyyMMddHHmmss}.txt";
 
+                var path = Path.Combine("wwwroot/files", fileName);
+                var productName = newProduct.Name;
+
+                System.IO.File.WriteAllText(path, productName);
+            }
         }
     }
 }
